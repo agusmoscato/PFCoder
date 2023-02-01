f7ee61bdc518fe4e93862276a4baa075c9874720
Agustin Moscato <agustinmoscato369@gmail.com>
2023-02-01 18:54:01 -0300

    Retoques visuales entrega 3

--- js/main.js
+++ js/main.js
@@ -12,26 +12,26 @@ class Producto {
 
 /* LISTA DE PRODUCTOS */
 const productos = [
-    new Producto(0, "cinzano", 100, "aperitivo", "./media/MENU/APERITIVOS/CINZANO.png"),
-    new Producto(1, "gancia", 100, "aperitivo", "./media/MENU/APERITIVOS/GANCIA.png"),
-    new Producto(2, "fernet", 100, "aperitivo", "./media/MENU/APERITIVOS/FERNET.png"),
-    new Producto(3, "aperol", 100, "aperitivo", "./media/MENU/APERITIVOS/APEROL.png"),
-    new Producto(4, "cynar", 100, "aperitivo", "./media/MENU/APERITIVOS/CYNAR.png"),
-    new Producto(5, "quilmes", 100, "cerveza", "./media/MENU/CERVEZAS/QUILMES.png"),
-    new Producto(6, "patagonia", 100, "cerveza", "./media/MENU/CERVEZAS/PATAGONIA.png"),
-    new Producto(7, "corona", 100, "cerveza", "./media/MENU/CERVEZAS/CORONA.png"),
-    new Producto(8, "andes", 100, "cerveza", "./media/MENU/CERVEZAS/ANDES.png"),
-    new Producto(9, "budweiser", 100, "cerveza", "./media/MENU/CERVEZAS/BUDWEISER.png"),
-    new Producto(10, "vodka smirnoff", 100, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/SMIRNOFF.png"),
-    new Producto(11, "vodka skyy", 100, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/SKYY.png"),
-    new Producto(12, "gin bosque", 100, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/BOSQUE.png"),
-    new Producto(13, "gin beefeater", 100, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/BEEFEATER.png"),
-    new Producto(14, "malibu", 100, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/MALIBU.png"),
-    new Producto(15, "pepsi", 100, "gaseosa", "./media/MENU/GASEOSAS/PEPSI.png"),
-    new Producto(16, "7up", 100, "gaseosa", "./media/MENU/GASEOSAS/7UP.png"),
-    new Producto(17, "mirinda", 100, "gaseosa", "./media/MENU/GASEOSAS/MIRINDA.png"),
-    new Producto(18, "paso de los toros", 100, "gaseosa", "./media/MENU/GASEOSAS/PASODELOSTOROS.png"),
-    new Producto(19, "h2o", 100, "gaseosa", "./media/MENU/GASEOSAS/H2O.png"),
+    new Producto(0, "cinzano", 710, "aperitivo", "./media/MENU/APERITIVOS/CINZANO.png"),
+    new Producto(1, "gancia", 950, "aperitivo", "./media/MENU/APERITIVOS/GANCIA.png"),
+    new Producto(2, "fernet", 2370, "aperitivo", "./media/MENU/APERITIVOS/FERNET.png"),
+    new Producto(3, "aperol", 1490, "aperitivo", "./media/MENU/APERITIVOS/APEROL.png"),
+    new Producto(4, "cynar", 1445, "aperitivo", "./media/MENU/APERITIVOS/CYNAR.png"),
+    new Producto(5, "quilmes", 360, "cerveza", "./media/MENU/CERVEZAS/QUILMES.png"),
+    new Producto(6, "patagonia", 870, "cerveza", "./media/MENU/CERVEZAS/PATAGONIA.png"),
+    new Producto(7, "corona", 720, "cerveza", "./media/MENU/CERVEZAS/CORONA.png"),
+    new Producto(8, "andes", 460, "cerveza", "./media/MENU/CERVEZAS/ANDES.png"),
+    new Producto(9, "budweiser", 530, "cerveza", "./media/MENU/CERVEZAS/BUDWEISER.png"),
+    new Producto(10, "vodka smirnoff", 1470, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/SMIRNOFF.png"),
+    new Producto(11, "vodka skyy", 1230, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/SKYY.png"),
+    new Producto(12, "gin bosque", 1650, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/BOSQUE.png"),
+    new Producto(13, "gin beefeater", 6300, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/BEEFEATER.png"),
+    new Producto(14, "malibu", 3250, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/MALIBU.png"),
+    new Producto(15, "pepsi", 490, "gaseosa", "./media/MENU/GASEOSAS/PEPSI.png"),
+    new Producto(16, "7up", 390, "gaseosa", "./media/MENU/GASEOSAS/7UP.png"),
+    new Producto(17, "mirinda", 420, "gaseosa", "./media/MENU/GASEOSAS/MIRINDA.png"),
+    new Producto(18, "paso de los toros", 430, "gaseosa", "./media/MENU/GASEOSAS/PASODELOSTOROS.png"),
+    new Producto(19, "h2o", 430, "gaseosa", "./media/MENU/GASEOSAS/H2O.png"),
 
 ]
 /* ----------------- */
@@ -90,11 +90,21 @@ filterButtons.forEach(filterButton => {
 
 // CARRITO DE COMPRAS
 // Mostrar/dejar de mostrar carrito
-const carritoBtn = document.getElementById('carrito')
 const cartMain = document.getElementById(`carritoMain`)
 
+const carritoBtn = document.getElementById('carrito')
 carritoBtn.addEventListener('click', () => {
     cartMain.classList.toggle("hidden");
+});
+
+const cerrarCarrito = document.getElementById(`cerrarCarrito`)
+cerrarCarrito.addEventListener('click', () => {
+    cartMain.classList.add("hidden");
+});
+
+const abrirCarrito = document.getElementById(`abrirCarrito`)
+abrirCarrito.addEventListener('click', () => {
+    cartMain.classList.remove("hidden");
 });
 
 // Inicializar carrito
@@ -152,8 +162,8 @@ function renderizarCarrito() {
                     <img src="${producto.img}" alt="">
                 </div>
                 <div class="carritoProducto">
-                    <h3><strong>${producto.nombre.toUpperCase()} </strong>  x${producto.cantidad}</h3>
-                    <h3>| $${producto.precio}</h3>
+                    <h3><strong>${producto.nombre.toUpperCase()} | $${producto.precio} </strong> </h3>
+                    <h2>x${producto.cantidad}</h2>
                 </div>`
         ;
         cartContent.append(item);
@@ -186,6 +196,7 @@ clearButton.addEventListener("click", () => {
     counterDisplay.innerHTML = counter;
     renderizarCarrito();
     renderizarPrecioTotal();
+    cartMain.classList.add("hidden");
 });
 
 
