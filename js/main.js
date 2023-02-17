0ae7eb9d6b88bb18f6ab25574512eac84a8f7d7e
Agustin Moscato <agustinmoscato369@gmail.com>
2023-02-17 14:57:01 -0300

    Agrego la lista de productos en un archivo json y los traigo con fetch

--- js/main.js
+++ js/main.js
@@ -1,47 +1,22 @@
-/* CONSTRUCTOR DE LOS PRODUCTOS */
-class Producto {
-    constructor(id, nombre, precio, categoria, img) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.categoria = categoria;
-        this.img = img;
-    }
-}
-/* ----------------- */
+// Productos almacenados en archivo JSON
+const url = "./js/productos.json";
 
-/* LISTA DE PRODUCTOS */
-const productos = [
-    new Producto(0, "cinzano", 710, "aperitivo", "./media/MENU/APERITIVOS/CINZANO.png"),
-    new Producto(1, "gancia", 950, "aperitivo", "./media/MENU/APERITIVOS/GANCIA.png"),
-    new Producto(2, "fernet", 2370, "aperitivo", "./media/MENU/APERITIVOS/FERNET.png"),
-    new Producto(3, "aperol", 1490, "aperitivo", "./media/MENU/APERITIVOS/APEROL.png"),
-    new Producto(4, "cynar", 1445, "aperitivo", "./media/MENU/APERITIVOS/CYNAR.png"),
-    new Producto(5, "quilmes", 360, "cerveza", "./media/MENU/CERVEZAS/QUILMES.png"),
-    new Producto(6, "patagonia", 870, "cerveza", "./media/MENU/CERVEZAS/PATAGONIA.png"),
-    new Producto(7, "corona", 720, "cerveza", "./media/MENU/CERVEZAS/CORONA.png"),
-    new Producto(8, "andes", 460, "cerveza", "./media/MENU/CERVEZAS/ANDES.png"),
-    new Producto(9, "budweiser", 530, "cerveza", "./media/MENU/CERVEZAS/BUDWEISER.png"),
-    new Producto(10, "vodka smirnoff", 1470, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/SMIRNOFF.png"),
-    new Producto(11, "vodka skyy", 1230, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/SKYY.png"),
-    new Producto(12, "gin bosque", 1650, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/BOSQUE.png"),
-    new Producto(13, "gin beefeater", 6300, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/BEEFEATER.png"),
-    new Producto(14, "malibu", 3250, "bebidaBlanca", "./media/MENU/BEBIDASBLANCAS/MALIBU.png"),
-    new Producto(15, "pepsi", 490, "gaseosa", "./media/MENU/GASEOSAS/PEPSI.png"),
-    new Producto(16, "7up", 390, "gaseosa", "./media/MENU/GASEOSAS/7UP.png"),
-    new Producto(17, "mirinda", 420, "gaseosa", "./media/MENU/GASEOSAS/MIRINDA.png"),
-    new Producto(18, "paso de los toros", 430, "gaseosa", "./media/MENU/GASEOSAS/PASODELOSTOROS.png"),
-    new Producto(19, "h2o", 430, "gaseosa", "./media/MENU/GASEOSAS/H2O.png"),
-]
-/* ----------------- */
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    localStorage.setItem('productos', JSON.stringify(data.productos));
+  })
+  .catch(error => console.error(error));
+
 
 // Enviar y devolver lista de productos del almacenamiento local
-localStorage.setItem("productos", JSON.stringify(productos));
-const productosAlmacenados = JSON.parse(localStorage.getItem("productos"));
+const productosAlmacenados = localStorage.getItem('productos');
+const misProductos = JSON.parse(productosAlmacenados);
+
 
 /* CREAR NODOS DE LOS PRODUCTOS Y PONERLOS EN EL DOM */
 const shopContent = document.getElementById("shopContent")
-productosAlmacenados.forEach((productos) => {
+misProductos.forEach((productos) => {
     let content = document.createElement('div');
     content.classList.add("col-sm-12", "col-lg-4", "all", `${productos.categoria}`);
     content.innerHTML = `
@@ -114,17 +89,17 @@ const guardarContador = () => {
 
 };
 
-
 const cartButtons = document.querySelectorAll(`.button`);
 const counterDisplay = document.querySelector('#numero');
-let counter = JSON.parse(localStorage.getItem("counter")) || 0;
+let counter = JSON.parse(localStorage.getItem("counter"));
+
 
 // Agregar producto al carrito
 cartButtons.forEach(cartButton => {
     cartButton.addEventListener("click", () => {
         const indexProductoSelec = Array.prototype.indexOf.call(cartButtons, cartButton);
 
-        const productoAgregar = productos.filter((producto) => producto.id === indexProductoSelec)[0];
+        const productoAgregar = misProductos.filter((producto) => producto.id === indexProductoSelec)[0];
         // contador del carrito
 
         counter++;
